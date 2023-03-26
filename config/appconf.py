92ac900971d5bf921c1a342b128a22d3b0e0b7a8
Gonzalo Vergara <grvergara@gmail.com>
2023-03-26 17:29:43 -0300

    add default connection string for postgres and redis

--- config/appconf.py
+++ config/appconf.py
@@ -1,8 +1,8 @@
 import os
 
 class AppConf:
-    db_url = os.environ.get('DATABASE_URL')
-    redis_url = os.environ.get('REDIS_URL')
+    db_url = os.environ.get('DATABASE_URL') or 'postgres://127.0.0.1/postgres'
+    redis_url = os.environ.get('REDIS_URL') or 'redis://127.0.0.1:6379/'
     rq_result_ttl = os.environ.get('RQ_RESULT_TTL') or 7*24*60*60
     rq_timeout = os.environ.get('RQ_TIMEOUT') or 30*60
     slack_callback = os.environ.get('SLACK_CALLBACK')
