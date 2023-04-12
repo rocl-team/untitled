6f0b05776aff01d0746d0447df17f9995aa673b8
Gonzalo Vergara <grvergara@gmail.com>
2023-04-11 22:35:06 -0400

    add load balancer test-url

--- api/app.py
+++ api/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def landing():
     return "Have a nice day! (1)", 200
+
+@app.route('/lb-poke', methods=['GET'])
+def lbPoke():
+    return "The second make you wonder", 200
